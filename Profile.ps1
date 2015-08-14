713644bb0fb48b2364805b3203e6d3d674754210
Clint Parker <clintcparker@gmail.com>
2015-08-14 14:39:33 -0700

    Update Profile.ps1

--- Profile.ps1
+++ Profile.ps1
@@ -1,4 +1,3 @@
-
 Clear-Host
 
 Set-Location C:\
@@ -8,10 +7,18 @@ $Shell.WindowTitle = $title
 $Profile_Path = Split-Path $profile
 $Profile_Module = "$Profile_Path\Profile.psm1"
 
-Write-Host "PROFILE SOURCE can be found @ https://raw.githubusercontent.com/clintcparker/PowerShellScripts/master/Profile.ps1"
+Write-Host "
+
+PROFILE SOURCE can be found @ https://raw.githubusercontent.com/clintcparker/PowerShellScripts/master/Profile.ps1
+
+
+Run `Get-Command -Module Profile` to see all profile functions.
+
+"
+
 
 if (-Not (Test-Path $Profile_Module))
 {
     Invoke-WebRequest "https://raw.githubusercontent.com/clintcparker/PowerShellScripts/master/Profile.psm1" -OutFile $Profile_Module
 }
-Import-Module $Profile_Module
+Import-Module $Profile_Module -Force
